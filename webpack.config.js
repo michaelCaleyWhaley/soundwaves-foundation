3003e39be116d17b3c9e14bd084e63a00cf42cc6
michaelCaleyWhaley <kneedeepwater@hotmail.com>
2023-07-05 19:21:24 +0100

    rejig folders

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,34 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
-const path = require("path");
+const path = require('path');
 
 // eslint-disable-next-line no-undef
 module.exports = {
-  mode: "production",
-  entry: "./src/index.ts",
+  mode: 'production',
+  entry: './src/index.ts',
   devtool: false,
   devServer: {
-    static: "./dist",
+    static: './dist',
   },
-  target: ["web", "es5"],
+  target: ['web', 'es5'],
   module: {
     rules: [
       {
         test: /\.s[ac]ss$/i,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
         test: /\.tsx?$/,
-        use: "ts-loader",
+        use: 'ts-loader',
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: ['.tsx', '.ts', '.js'],
   },
   output: {
-    filename: "bundle.js",
+    filename: 'bundle.js',
     // eslint-disable-next-line no-undef
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, 'dist'),
   },
 };
